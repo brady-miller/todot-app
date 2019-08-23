69a45537203f2845b1680f75ab934f6c6c79338f
brady-miller <46899559+brady-miller@users.noreply.github.com>
2019-08-22 20:23:34 -0500

    (WIP) Alert Feature

---
+++ src/components/common/Alert.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="red-alert" v-if="isError">
+        <p><span>!</span> ERROR: {{ getError }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator';
+
+@Component
+export default class Alert extends Vue {
+    public get isError(): boolean {
+        return this.$store.getters.isError;
+    }
+
+    public get getError(): string {
+        return this.$store.getters.getError;
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+.red-alert {
+    width: 75%;
+    background-color: red;
+    border: 1px solid red;
+    border-radius: 5px;
+
+    p {
+        color: #ffffff;
+    }
+
+    span {
+        font-size: 1.2em;
+    }
+}
+
+</style>
